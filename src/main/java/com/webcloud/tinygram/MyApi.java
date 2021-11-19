ac66ffda7d1ef5e0ae3001a2052e44ff87161328
Thomas Lapierre <thomaslapierre@orange.fr>
2021-11-19 12:43:57 +0100

    debug add like

--- src/main/java/com/webcloud/tinygram/MyApi.java
+++ src/main/java/com/webcloud/tinygram/MyApi.java
@@ -181,12 +181,15 @@ public class MyApi {
 
         Transaction txn = datastore.beginTransaction();
         Entity e = datastore.get(key);
+
+        /**
         ArrayList<String> personWhoLike = (ArrayList<String>) e.getProperty("listeAime");
         if(personWhoLike==null){
             personWhoLike = new ArrayList<String>();
         }
         personWhoLike.add(like.emailUserQuiLike);
-        e.setProperty("listeAime",personWhoLike);
+        e.setProperty("listeAime",personWhoLike);*/
+
         long likes = (long) e.getProperty("likes") + 1;
         e.setProperty("likes", likes);
         datastore.put(e);
